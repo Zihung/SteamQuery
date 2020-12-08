3bce5b3554e5a0593fff3e31212327fdf1f3bd6b
Duc Truong <70175176+dhtruong@users.noreply.github.com>
2020-12-07 21:13:04 -0800

    Added bottom navigation menu

--- app/src/main/java/com/bignerdranch/android/cua/NewsFeedActivity.kt
+++ app/src/main/java/com/bignerdranch/android/cua/NewsFeedActivity.kt
@@ -3,12 +3,19 @@ package com.bignerdranch.android.cua
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.Fragment
 import com.bignerdranch.android.cua.api.MyPojo
 import com.bignerdranch.android.cua.api.getJsonDataFromAsset
 
 import com.google.gson.Gson
+import kotlinx.android.synthetic.main.activity_new_feed.*
 
 class NewsFeedActivity : AppCompatActivity() {
+
+    private val gamesFollowedListFragment = GamesFollowedListFragment()
+    private val newsFeedFragment = NewsFeedFragment()
+    private val infoFragment = InfoFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_feed)
@@ -39,11 +46,20 @@ class NewsFeedActivity : AppCompatActivity() {
                 .commit()
         }
 
-
-
-
-
-
-
+        bottom_navigation.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.ic_home -> replaceFragment(newsFeedFragment)
+                R.id.ic_favorite -> replaceFragment(gamesFollowedListFragment)
+                R.id.ic_info -> replaceFragment(infoFragment)
+            }
+            true
+        }
+    }
+    private fun replaceFragment(fragment: Fragment){
+        if(fragment != null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragmentContainer, fragment)
+            transaction.commit()
+        }
     }
 }
